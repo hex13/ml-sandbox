659477dcff55034a913a76a4a6c2bb417c8e4c1b
Łukasz Lityński <lukasz.litynski@gmail.com>
2020-09-23 23:04:07 +0200

    use Maps ans Scatter

--- src/App.js
+++ src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 import { useLinearRegression } from './useLinearRegression';
 import { useAverage } from './useAverage';
 
+import { Maps } from './Maps';
+import { Scatter } from './Scatter';
+
 const methods = [useLinearRegression, useAverage];
 
 function App() {
   const [idx, setIdx] = React.useState(0);
   return (
     <div className="App">
+       {/* <Maps />
+       <Scatter />  */}
       <button onClick={() => setIdx(idx => (idx + methods.length- 1) % methods.length )}>prev</button>
       <button onClick={() => setIdx(idx => (idx + 1) % methods.length)}>next</button>
       <Sandbox usePredict={methods[idx]}/>
